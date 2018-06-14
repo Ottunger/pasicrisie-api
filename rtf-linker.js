8adb227e11d58dd335330ea5d953634564d8efde
Mathonet Grégoire <greg.mathonet@gmail.com>
2018-06-14 22:09:17 +0200

    Begin link

--- rtf-linker.js
+++ rtf-linker.js
@@ -5,6 +5,7 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 
 const foldersToTreatMatcher = /\/blue\//;
+const baseS3Url = 'https://s3.eu-west-2.amazonaws.com/';
 
 exports.handler = (event, context, callback) => {
     const bucket = event.Records[0].s3.bucket.name, key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
@@ -20,8 +21,11 @@ exports.handler = (event, context, callback) => {
             callback(err);
             return;
         }
-        const rtf = data.Body.toString();
+        let rtf = data.Body.toString();
         // TODO: add links and find some keywords and name of book and author
+        rtf = rtf.replace(/[0-9]{5}/g, t => '{\\colortbl ;\\red0\\green0\\blue238;}\n'
+            + '{\\field{\\*\\fldinst HYPERLINK "' + baseS3Url + bucket
+            + '/' + key.replace(/[0-9]{5}/, t) + '"}{\\fldrslt{\\ul\\cf1 ' + t + '}}}');
         s3.putObject({
             Body: rtf,
             Bucket: bucket.replace('raw', 'linked'),
